874f5e16d9017337bd3c486d086753af4fd358b3
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-26 03:53:04 +0000

    Add scraper functionality

--- lib/gamespot_review/game.rb
+++ lib/gamespot_review/game.rb
@@ -1,5 +1,13 @@
 class GamespotReview::Game
-  attr_accessor :title, :score, :game_url, :release_console, :release_date, :reviewer, :review_date
+  attr_accessor :title, :score, :game_url, :release_console, :release_date, :reviewer, :review_date, :review_text
+  @@all = []
 
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- lib/gamespot_review/scraper.rb
+++ lib/gamespot_review/scraper.rb
@@ -1,3 +1,37 @@
-class GamespotReview::Scaper
+class GamespotReview::Scraper
+
+  def get_page_info(url)
+    Nokogiri::HTML(open(url))
+  end
+
+  def get_game_info
+    base_url = "https://www.gamespot.com/reviews/?page="
+    page_count = 1
+    5.times do
+      game_info = self.get_page_info(base_url + "#{page_count}").css("article.media.media-game")
+      self.create_games(game_info)
+
+      # release_console = game_info.css(
+      # release_date = game_info.css(
+      # reviewer = game_info.css(
+      # review_date = game_info.css(
+      # review_text = game_info.css(
+      page_count += 1
+    end
+  end
+
+  def create_games(game_info)
+    game_info.each do |game_info|
+      game = GamespotReview::Game.new
+      game.title = game_info.css("h3.media-title").text
+      game.score = game_info.css("span.content").text
+      game.game_url = "https://www.gamespot.com" + game_info.css("a").first["href"]
+      # game.release_console =
+      # game.release_date =
+      # game.reviewer =
+      # game.review_date =
+      # game.review_text =
+    end
+  end
 
 end
