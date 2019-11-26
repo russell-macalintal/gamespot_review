e6509ab14b0cf14c4853272b3c5147257ffb9a9f
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-26 04:24:06 +0000

    Refactored Scraper functionality

--- lib/gamespot_review/scraper.rb
+++ lib/gamespot_review/scraper.rb
@@ -7,31 +7,36 @@ class GamespotReview::Scraper
   def get_game_info
     base_url = "https://www.gamespot.com/reviews/?page="
     page_count = 1
+    game_info = []
     5.times do
-      game_info = self.get_page_info(base_url + "#{page_count}").css("article.media.media-game")
-      self.create_games(game_info)
-
-      # release_console = game_info.css(
-      # release_date = game_info.css(
-      # reviewer = game_info.css(
-      # review_date = game_info.css(
-      # review_text = game_info.css(
+      game_info << self.get_page_info(base_url + "#{page_count}").css("article.media.media-game")
+      game_info.flatten!
       page_count += 1
     end
+    game_info
   end
 
-  def create_games(game_info)
-    game_info.each do |game_info|
+  def create_games
+    self.get_game_info.each do |game_info|
       game = GamespotReview::Game.new
       game.title = game_info.css("h3.media-title").text
       game.score = game_info.css("span.content").text
       game.game_url = "https://www.gamespot.com" + game_info.css("a").first["href"]
+    end
+  end
+
+  def add_game_info
+    GamespotReview::Game.all.each do |game|
       # game.release_console =
       # game.release_date =
       # game.reviewer =
       # game.review_date =
       # game.review_text =
     end
+  end
+
+  def list_games
+
   end
 
 end
